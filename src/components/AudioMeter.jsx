ce1cc285a04d9977883e9747095ec247e3e561c7
Kevin Ishimwe <ishimwekevin45@gmail.com>
2024-06-29 23:52:32 -0400

    add

--- src/components/AudioMeter.jsx
+++ src/components/AudioMeter.jsx
@@ -1,21 +1,101 @@
 import React, { useState, useEffect, useRef } from "react";
 import AudioVisualizer from "./AudioVisualizer";
+import { FaMicrophone } from "react-icons/fa";
 
 const key1 = import.meta.env.VITE_OPENAI_API_KEY;
 
+function updateChatHistory(userPrompt, modelResponse) {
+  const storageKey = "ChatHistory";
+  let chatHistory = JSON.parse(localStorage.getItem(storageKey)) || [];
+
+  const userEntry = {
+    role: "user",
+    parts: [{ text: userPrompt }],
+  };
+
+  const modelEntry = {
+    role: "model",
+    parts: [{ text: modelResponse }],
+  };
+  chatHistory.push(userEntry, modelEntry);
+  localStorage.setItem(storageKey, JSON.stringify(chatHistory));
+}
+const generativeCompletion = async (prompt) => {
+  console.log(prompt);
+  try {
+    const chatResponse = await fetch("http://localhost:2020/conversation", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt, history: [] }),
+    }).then((res) => res.json());
+
+    const modelResponse = await chatResponse.response;
+    updateChatHistory(prompt, modelResponse);
+    return modelResponse;
+  } catch (err) {
+    console.log("Generative completion failed:", err);
+  }
+};
 function AudioLevelMeter() {
   const [isTranscribing, setIsTranscribing] = useState(false);
   const [audioLevel, setAudioLevel] = useState(0);
   const [isSpeaking, setIsSpeaking] = useState(null);
+  const [isRecording, setIsRecording] = useState(false);
   // Refs
   const mediaRecorderRef = useRef(null);
   const audioContextRef = useRef(null);
   const analyserRef = useRef(null);
   const animationFrameRef = useRef(null);
   const silenceTimerRef = useRef(null);
-  const SILENCE_THRESHOLD = 1.1;
-  const SILENCE_DURATION = 2000; // 1 second in milliseconds
+  const SILENCE_THRESHOLD = 1.3;
+  const SILENCE_DURATION = 1200; // 1 second in milliseconds
 
+  const handleGenerativeResponse = async (transcribedText) => {
+    console.log(transcribedText);
+    const generativeText = await generativeCompletion(transcribedText);
+    const textChunks = generativeText.trim().split("\n");
+    console.log(textChunks);
+    const playAudioChunks = async (chunks, index = 0) => {
+      if (index >= chunks.length) return setIsSpeaking(false);
+      setIsSpeaking(true);
+      setIsTranscribing(false);
+      const ttsResponse = await fetch(
+        "https://api.openai.com/v1/audio/speech",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${key1}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            model: "tts-1",
+            input: chunks[index],
+            voice: "alloy",
+          }),
+        }
+      );
+      if (!ttsResponse.ok) {
+        const errorDetails = await ttsResponse.json();
+        setIsSpeaking(false);
+        throw new Error(
+          `TTS API request failed: ${ttsResponse.statusText} - ${errorDetails.error.message}`
+        );
+      }
+      const ttsBlob = await ttsResponse.blob();
+      const audioUrl = URL.createObjectURL(ttsBlob);
+      const audioElement = new Audio(audioUrl);
+
+      audioElement.play();
+      audioElement.addEventListener("ended", () => {
+        playAudioChunks(chunks, index + 1);
+      });
+    };
+
+    playAudioChunks(textChunks);
+  };
   useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({ audio: true, video: false })
@@ -25,15 +105,17 @@ function AudioLevelMeter() {
         });
         let chunkz = [];
         mediaRecorderRef.current.ondataavailable = (e) => {
-
-          if (e.data.size > 50000) {
+            console.log(e.data.size)
+          if (e.data.size > 45000) {
             chunkz.push(e.data);
           }
         };
         mediaRecorderRef.current.onstop = () => {
           if (chunkz.length > 0) {
             const audioBlob = new Blob(chunkz, { type: "audio/webm" });
+
             handleTranscription(audioBlob);
+
             chunkz = [];
           }
         };
@@ -73,7 +155,7 @@ function AudioLevelMeter() {
     setAudioLevel(normalizedAverage);
 
     if (normalizedAverage > SILENCE_THRESHOLD) {
-      setIsSpeaking(true);
+      setIsRecording(true);
       if (silenceTimerRef.current) {
         clearTimeout(silenceTimerRef.current);
         silenceTimerRef.current = null;
@@ -81,22 +163,23 @@ function AudioLevelMeter() {
     } else if (!silenceTimerRef.current) {
       // Start the silence timer if it's not already running
       silenceTimerRef.current = setTimeout(() => {
-        setIsSpeaking(false);
+        setIsRecording(false);
         silenceTimerRef.current = null;
       }, SILENCE_DURATION);
     }
 
     animationFrameRef.current = requestAnimationFrame(measureAudioLevel);
   };
+
   useEffect(() => {
     if (mediaRecorderRef.current) {
-      if (isSpeaking) {
+      if (isRecording) {
         mediaRecorderRef.current.start();
-      } else if (!isSpeaking) {
+      } else if (!isRecording) {
         mediaRecorderRef.current.stop();
       }
     }
-  }, [isSpeaking]);
+  }, [isRecording]);
 
   const handleTranscription = async (audioBlob) => {
     setIsTranscribing(true);
@@ -118,10 +201,10 @@ function AudioLevelMeter() {
       );
 
       const data = await response.json();
-      console.log(data);
+      console.log(data.text);
       // Handle transcription result here
-
       setIsTranscribing(false);
+      handleGenerativeResponse(data.text);
     } catch (error) {
       console.error("Error during transcription:", error);
       setIsTranscribing(false);
@@ -132,7 +215,19 @@ function AudioLevelMeter() {
     <div>
       <p>Current Audio Level: {audioLevel.toFixed(2)}</p>
       <p>Status: {isSpeaking ? "Speaking" : "Silent"}</p>
-      <AudioVisualizer voice={isSpeaking} fetching={isTranscribing} />
+      <AudioVisualizer
+        aiVoice={isSpeaking}
+        fetching={isTranscribing}
+        voice={isRecording}
+      />
+      <FaMicrophone
+        className={`mx-auto mt-12 text-4xl text-indigo-800 h-[2em] w-[2em] py-4 rounded-full bg-white shadow-lg ${
+          isRecording
+            ? "text-red-600"
+            : "hover:text-indigo-600 hover:scale-[1.02]"
+        }`}
+        // onClick=  ///stop everything 
+      />
     </div>
   );
 }
--- src/pages/VoiceTherapy.jsx
+++ src/pages/VoiceTherapy.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { IoMdInformationCircleOutline } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
-import { FaMicrophone } from "react-icons/fa";
+
 import Logo_max from "../components/Logo-max";
 import AudioVisualizer from "../components/AudioVisualizer";
 
@@ -23,138 +23,6 @@ function updateChatHistory(userPrompt, modelResponse) {
   localStorage.setItem(storageKey, JSON.stringify(chatHistory));
 }
 function VoiceTherapy() {
-  // State for tracking different phases
-  const [isRecording, setIsRecording] = useState(false);
-  const [isTranscribing, setIsTranscribing] = useState(false);
-  const [isSpeaking, setIsSpeaking] = useState(false);
-  const [audioChunks, setAudioChunks] = useState([]);
-  const [mediaRecorder, setMediaRecorder] = useState(null);
-
-  useEffect(() => {
-    if (isRecording && mediaRecorder) {
-      mediaRecorder.start();
-      mediaRecorder.ondataavailable = (event) => {
-        setAudioChunks((prev) => [...prev, event.data]);
-      };
-    } else if (mediaRecorder) {
-      mediaRecorder.stop();
-      if (mediaRecorder.stream) {
-        mediaRecorder.stream.getTracks().forEach((track) => track.stop());
-      }
-    }
-  }, [isRecording, mediaRecorder]);
-
-  useEffect(() => {
-    if (!isRecording && audioChunks.length > 0) {
-      const audioBlob = new Blob(audioChunks, { type: "audio/wav" });
-      handleTranscription(audioBlob);
-      setAudioChunks([]);
-    }
-  }, [isRecording, audioChunks]);
-
-  const generativeCompletion = async (prompt) => {
-    try {
-      const chatResponse = await fetch("http://localhost:2020/conversation", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ prompt, history: [] }),
-      }).then((res) => res.json());
-
-      const modelResponse = await chatResponse.response;
-      updateChatHistory(prompt, modelResponse);
-      return modelResponse;
-    } catch (err) {
-      console.log("Generative completion failed:", err);
-    }
-  };
-
-  const handleTranscription = async (audioBlob) => {
-    setIsTranscribing(true);
-    const formData = new FormData();
-    const audioFile = new File([audioBlob], "audio.webm", {
-      type: "audio/webm",
-    });
-    formData.append("file", audioFile);
-    formData.append("model", "whisper-1");
-
-    try {
-      const response = await fetch(
-        "https://api.openai.com/v1/audio/transcriptions",
-        {
-          method: "POST",
-          headers: { Authorization: `Bearer ${key1}` },
-          body: formData,
-        }
-      );
-
-      const data = await response.json();
-
-      await handleGenerativeResponse(data.text);
-    } catch (error) {
-      console.error("Error during transcription:", error);
-      setIsTranscribing(false);
-    }
-  };
-
-  const handleGenerativeResponse = async (transcribedText) => {
-    const generativeText = await generativeCompletion(transcribedText);
-    const textChunks = generativeText.trim().split("\n");
-    console.log(textChunks);
-    const playAudioChunks = async (chunks, index = 0) => {
-      if (index >= chunks.length) return setIsSpeaking(false);
-      setIsSpeaking(true);
-      setIsTranscribing(false);
-      const ttsResponse = await fetch(
-        "https://api.openai.com/v1/audio/speech",
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${key1}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            model: "tts-1",
-            input: chunks[index],
-            voice: "alloy",
-          }),
-        }
-      );
-      if (!ttsResponse.ok) {
-        const errorDetails = await ttsResponse.json();
-        setIsSpeaking(false);
-        throw new Error(
-          `TTS API request failed: ${ttsResponse.statusText} - ${errorDetails.error.message}`
-        );
-      }
-      const ttsBlob = await ttsResponse.blob();
-      const audioUrl = URL.createObjectURL(ttsBlob);
-      const audioElement = new Audio(audioUrl);
-
-      audioElement.play();
-      audioElement.addEventListener("ended", () => {
-        playAudioChunks(chunks, index + 1);
-      });
-    };
-
-    playAudioChunks(textChunks);
-  };
-  const handleRecord = () => {
-    if (isRecording) {
-      setIsRecording(false);
-    } else {
-      navigator.mediaDevices
-        .getUserMedia({ audio: true })
-        .then((stream) => {
-          const newMediaRecorder = new MediaRecorder(stream);
-          setMediaRecorder(newMediaRecorder);
-          setIsRecording(true);
-        })
-        .catch((err) => console.error("Error accessing microphone", err));
-    }
-  };
 
   return (
     <div className="h-[100vh] bg-[#F9F9F9] relative overflow-hidden">
@@ -166,19 +34,7 @@ function VoiceTherapy() {
         <Logo_max orientation={true} />
       </div>
       <div className="absolute mx-auto grid w-full justify-center">
-        <AudioVisualizer
-          voice={isRecording}
-          fetching={isTranscribing}
-          aiVoice={isSpeaking}
-        />
-        <FaMicrophone
-          className={`mx-auto mt-12 text-4xl text-indigo-800 h-[2em] w-[2em] py-4 rounded-full bg-white shadow-lg ${
-            isRecording
-              ? "text-red-600"
-              : "hover:text-indigo-600 hover:scale-[1.02]"
-          }`}
-          onClick={handleRecord}
-        />
+   
       </div>
     </div>
   );
